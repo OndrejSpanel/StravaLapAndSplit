f84591e2af38412a32700403c2e43d6e7f5b0afc
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2016-06-16 22:29:51 +0200

    Reorder functions by execution order.

--- src/com/github/opengrabeso/stravalas/Main.scala
+++ src/com/github/opengrabeso/stravalas/Main.scala
@@ -18,9 +18,9 @@ object Main {
 
   def doComputation(i: Int): String = ("*" + i.toString) * 2
 
-  def getLapsFrom(authToken: String, id: String): Array[Double] = {
-
-    Array(0.0, 0.5, 1.0)
+  def secret: String = {
+    val secretStream = Main.getClass.getResourceAsStream("/secret.txt")
+    scala.io.Source.fromInputStream(secretStream).mkString
   }
 
   def stravaAuth(code: String): String = {
@@ -45,8 +45,9 @@ object Main {
 
   }
 
-  def secret: String = {
-    val secretStream = Main.getClass.getResourceAsStream("/secret.txt")
-    scala.io.Source.fromInputStream(secretStream).mkString
+  def getLapsFrom(authToken: String, id: String): Array[Double] = {
+
+    Array(0.0, 0.5, 1.0)
   }
+
 }
