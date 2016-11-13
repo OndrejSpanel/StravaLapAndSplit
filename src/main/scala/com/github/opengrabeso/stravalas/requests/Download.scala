b6d1e3c669d7b19214085b46687aeaeb6c881ce7
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2016-11-13 16:55:28 +0100

    Strava API connected, not working (Apache API unsupported on Google App Engine).

--- src/main/scala/com/github/opengrabeso/stravalas/requests/Download.scala
+++ src/main/scala/com/github/opengrabeso/stravalas/requests/Download.scala
@@ -3,72 +3,19 @@ package requests
 
 import spark.{Request, Response}
 
-object Download extends DefineRequest {
-
+object Download extends ProcessFile {
   def handle = Handle("/download")
 
-  override def html(req: Request, resp: Response) = {
-
-    val id = req.queryParams("id")
-    val op = req.queryParams("operation")
-    val authToken = req.queryParams("auth_token")
-
+  def process(req: Request, resp: Response, export: Array[Byte], filename: String): Unit = {
     val contentType = "application/octet-stream"
 
-    def download(export: Array[Byte], filename: String): Unit = {
-      resp.status(200)
-      resp.header("Content-Disposition", filename)
-      resp.`type`(contentType)
+    resp.status(200)
+    resp.header("Content-Disposition", filename)
+    resp.`type`(contentType)
 
-      val out = resp.raw.getOutputStream
-      out.write(export)
-    }
-
-    op match {
-      case "split" =>
-        val eventsInput = req.raw.getParameterValues("events")
-        val splitTime = req.queryParams("time").toInt
-        val session = req.session
-
-        val events = session.attribute("events-" + id).asInstanceOf[Main.ActivityEvents]
-
-        val adjusted = Main.adjustEvents(events, eventsInput)
-
-        val split = adjusted.split(splitTime)
-
-        split.foreach{ save =>
-
-          val export = FitExport.export(save)
-
-          download(export, s"attachment;filename=split_${id}_$splitTime.fit")
-        }
-
-        Nil
-
-      case "process" =>
-
-        val eventsInput = req.raw.getParameterValues("events")
-
-        val events = Main.getEventsFrom(authToken, id)
-
-        val adjusted = Main.adjustEvents(events, eventsInput)
-
-        val export = FitExport.export(adjusted)
-
-        download(export, s"attachment;filename=split_$id.fit")
-
-        Nil
-
-      case "copy" =>
-        val exportUri = s"https://www.strava.com/activities/$id/export_tcx"
-        /*
-        val dispatcher = req.getRequestDispatcher(exportUri)
-        dispatcher.forward(req, resp)
-        */
-        resp.redirect(exportUri)
-
-        Nil
-
-    }
+    val out = resp.raw.getOutputStream
+    out.write(export)
   }
+
+
 }
--- src/main/scala/com/github/opengrabeso/stravalas/requests/UploadToStrava.scala
+++ src/main/scala/com/github/opengrabeso/stravalas/requests/UploadToStrava.scala
@@ -1,13 +1,27 @@
 package com.github.opengrabeso.stravalas
 package requests
 
+import net.suunto3rdparty.strava.StravaAPI
 import spark.{Request, Response}
 
-object UploadToStrava extends DefineRequest with ActivityRequestHandler {
+object UploadToStrava extends ProcessFile {
   def handle = Handle("/upload-strava")
 
-  override def html(request: Request, resp: Response) = {
-    ???
+  def process(req: Request, resp: Response, export: Array[Byte], filename: String): Unit = {
+    val authToken = req.queryParams("auth_token")
+
+    val api = new StravaAPI(authToken)
+
+    api.uploadRawFileGz(export, ".fit.gz") // TODO: forward response (at least status)
+
+    val contentType = "application/octet-stream"
+    resp.status(200)
+    resp.header("Content-Disposition", filename)
+    resp.`type`(contentType)
+
+    val out = resp.raw.getOutputStream
+    out.write(export)
   }
+
 
 }
