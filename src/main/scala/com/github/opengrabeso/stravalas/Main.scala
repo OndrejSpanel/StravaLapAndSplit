OndrejSpanel/StravaLapAndSplit

commit 388e86c4eb31dadffeef804326006c421556d7a1

Get rid of Stamp, use ZonedDateTime instead.

src/main/scala/com/github/opengrabeso/stravalas/Main.scala
@@ -151,7 +151,7 @@ object Main {
 
       val ees = (events, events.drop(1) :+ events.last, sports zip sportChange).zipped.map { case (e1, e2, (sport,change)) =>
         val action = if (change) "split" else e1.defaultEvent
-        EditableEvent(action, e1.stamp.secondsFrom(id.startTime), distanceForTime(e1.stamp.aTime), sport)
+        EditableEvent(action, id.secondsInActivity(e1.stamp), distanceForTime(e1.stamp), sport)
       }
 
       // consolidate mutliple events with the same time so that all of them have the same action
@@ -179,22 +179,22 @@ object Main {
 
       val splitEvents = events.filter(_.isSplit).toSeq
 
-      val splitTimes = splitEvents.map(e => e.stamp.aTime)
+      val splitTimes = splitEvents.map(e => e.stamp)
 
       assert(splitTimes.contains(id.startTime))
       assert(splitTimes.contains(id.endTime))
 
       val splitRanges = splitEvents zip splitTimes.tail
 
-      val toSplit = splitRanges.find(_._1.stamp.secondsFrom(id.startTime) == splitTime)
+      val toSplit = splitRanges.find(t => secondsInActivity(t._2) == splitTime)
 
       toSplit.map { case (beg, endTime) =>
 
 
-        val begTime = beg.stamp.aTime
+        val begTime = beg.stamp
 
 
-        val eventsRange = (events zip sports).dropWhile(_._1.stamp.aTime <= begTime).takeWhile(_._1.stamp.aTime < endTime)
+        val eventsRange = (events zip sports).dropWhile(_._1.stamp <= begTime).takeWhile(_._1.stamp < endTime)
 
         val indexBeg = times.lastIndexWhere(_ <= begTime) max 0
 
@@ -230,14 +230,12 @@ object Main {
   }
 
 
-  def processActivityStream(actId: ActivityId, act: ActivityStreams, laps: List[Stamp], segments: Seq[Event]): ActivityEvents = {
-
-    val pauses: Seq[(Int, Stamp)] = Nil
+  def processActivityStream(actId: ActivityId, act: ActivityStreams, laps: List[ZonedDateTime], segments: Seq[Event]): ActivityEvents = {
 
     // TODO: provide activity type with the split
-    val events = (BegEvent(Stamp(actId.startTime)) +: EndEvent(Stamp(actId.endTime)) +: laps.map(LapEvent)) ++ segments
+    val events = (BegEvent(actId.startTime) +: EndEvent(actId.endTime) +: laps.map(LapEvent)) ++ segments
 
-    val eventsByTime = events.sortBy(_.stamp.aTime)
+    val eventsByTime = events.sortBy(_.stamp)
 
     val sports = eventsByTime.map(x => actId.sportName)
 
@@ -306,8 +304,6 @@ object Main {
 
     }
 
-    val act = StravaActivityStreams
-
     val laps = {
 
 
@@ -323,9 +319,7 @@ object Main {
       }).toList
 
 
-      val lapsInSeconds = lapTimes.map(lap => Seconds.secondsBetween(startTime, lap).getSeconds)
-
-      lapTimes.filter(_ > actId.startTime).map(Stamp.apply)
+      lapTimes.filter(_ > actId.startTime)
     }
 
     val segments: Seq[Event] = {
@@ -336,8 +330,8 @@ object Main {
         val segDuration = seg.path("elapsed_time").intValue
         val segPrivate = seg.path("segment").path("private").booleanValue
         Seq(
-          StartSegEvent(segName, segPrivate, Stamp(segStartTime)),
-          EndSegEvent(segName, segPrivate, Stamp(segStartTime).offset(segDuration))
+          StartSegEvent(segName, segPrivate, segStartTime),
+          EndSegEvent(segName, segPrivate, segStartTime.withDurationAdded(segDuration, 1000))
         )
       }
     }

src/main/scala/com/github/opengrabeso/stravalas/Event.scala
@@ -1,20 +1,13 @@
 package com.github.opengrabeso.stravalas
-import org.joda.time.{DateTime => ZonedDateTime, Period, Seconds}
-
-case class Stamp(aTime: ZonedDateTime) {
-  def offset(t: Int) = Stamp(aTime.withDurationAdded(t, 1000))
-
-  def secondsFrom(beg: ZonedDateTime): Int = Seconds.secondsBetween(beg, aTime).getSeconds
-}
+import org.joda.time.{DateTime => ZonedDateTime}
 
 case class EventKind(id: String, display: String)
 
 sealed abstract class Event {
-  def stamp: Stamp
+  def stamp: ZonedDateTime
   def description: String
   def isSplit: Boolean
 
-  def id: String = stamp.aTime.toString
   def defaultEvent: String
 
   protected def listSplitTypes: Seq[EventKind] = Seq(
@@ -54,23 +47,23 @@ object Events {
 
 }
 
-case class PauseEvent(duration: Int, stamp: Stamp) extends Event {
+case class PauseEvent(duration: Int, stamp: ZonedDateTime) extends Event {
   def description = s"Pause ${Events.niceDuration(duration)}"
   def defaultEvent = if (duration>=15) "lap" else ""
   def isSplit = false
 }
-case class PauseEndEvent(duration: Int, stamp: Stamp) extends Event {
+case class PauseEndEvent(duration: Int, stamp: ZonedDateTime) extends Event {
   def description = s"Pause end"
   def defaultEvent = if (duration >= 30) "lap" else ""
   def isSplit = false
 }
-case class LapEvent(stamp: Stamp) extends Event {
+case class LapEvent(stamp: ZonedDateTime) extends Event {
   def description = "Lap"
   def defaultEvent = "lap"
   def isSplit = false
 }
 
-case class EndEvent(stamp: Stamp) extends Event {
+case class EndEvent(stamp: ZonedDateTime) extends Event {
   def description = "End"
   def defaultEvent = "end"
   def isSplit = true
@@ -78,7 +71,7 @@ case class EndEvent(stamp: Stamp) extends Event {
   override def listTypes: Array[EventKind] = Array(EventKind("", "--"))
 }
 
-case class BegEvent(stamp: Stamp) extends Event {
+case class BegEvent(stamp: ZonedDateTime) extends Event {
   def description = "<b>*** Start activity</b>"
   def defaultEvent = "split"
   def isSplit = true
@@ -86,7 +79,7 @@ case class BegEvent(stamp: Stamp) extends Event {
   override def listTypes = listSplitTypes.toArray
 }
 
-case class SplitEvent(stamp: Stamp) extends Event {
+case class SplitEvent(stamp: ZonedDateTime) extends Event {
   def description = "Split"
   def defaultEvent = "split"
   def isSplit = true
@@ -102,12 +95,12 @@ trait SegmentTitle {
 
 }
 
-case class StartSegEvent(name: String, isPrivate: Boolean, stamp: Stamp) extends Event with SegmentTitle {
+case class StartSegEvent(name: String, isPrivate: Boolean, stamp: ZonedDateTime) extends Event with SegmentTitle {
   def description: String = s"Start $title"
   def defaultEvent = ""
   def isSplit = false
 }
-case class EndSegEvent(name: String, isPrivate: Boolean, stamp: Stamp) extends Event with SegmentTitle {
+case class EndSegEvent(name: String, isPrivate: Boolean, stamp: ZonedDateTime) extends Event with SegmentTitle {
   def description: String = s"End $title"
   def defaultEvent = ""
   def isSplit = false

src/main/scala/com/github/opengrabeso/stravalas/FitExport.scala
@@ -118,7 +118,7 @@ object FitExport {
 
     val lapsAsEvents = events.events.flatMap {
       case LapEvent(time) =>
-        Some(new LapEvent(time.aTime))
+        Some(new LapEvent(time))
       case _ =>
         None
     }

src/main/scala/com/github/opengrabeso/stravalas/requests/ActivityPage.scala
@@ -56,14 +56,14 @@ trait ActivityRequestHandler {
         lastSport = ee.sport
         <tr>
           <td> {xml.Unparsed(t.description)} </td>
-          <td> {Main.displaySeconds(t.stamp.secondsFrom(startTime))} </td>
-          <td> {Main.displayDistance(activityData.distanceForTime(t.stamp.aTime))} </td>
+          <td> {Main.displaySeconds(activityData.secondsInActivity(t.stamp))} </td>
+          <td> {Main.displayDistance(activityData.distanceForTime(t.stamp))} </td>
           <td> {sport} </td>
           <td>
             {val types = t.listTypes
-          if (types.length != 1 && !lastTime.contains(t.stamp.aTime)) {
-            lastTime = Some(t.stamp.aTime)
-            <select id={t.stamp.secondsFrom(startTime).toString} name="events" onchange="changeEvent(this, this.options[this.selectedIndex].value)">
+          if (types.length != 1 && !lastTime.contains(t.stamp)) {
+            lastTime = Some(t.stamp)
+            <select id={activityData.secondsInActivity(t.stamp).toString} name="events" onchange="changeEvent(this, this.options[this.selectedIndex].value)">
               {for (et <- types) yield {
               <option value={et.id} selected={if (action == et.id) "" else null}>
                 {et.display}
@@ -75,7 +75,7 @@ trait ActivityRequestHandler {
               <input type="hidden" name="events" value={t.defaultEvent}/>
           }}
           </td>
-          <td class="cellNoBorder" id={s"link${t.stamp.secondsFrom(startTime).toString}"}> </td>
+          <td class="cellNoBorder" id={s"link${activityData.secondsInActivity(t.stamp).toString}"}> </td>
         </tr>
       }}
     </table>

commit f4420697df38674b173d703c71764db052a695c8

Fixed split / download bug (introduced in 388e86c4eb31dadffeef804326006c421556d7a1).

src/main/scala/com/github/opengrabeso/stravalas/Main.scala
@@ -191,7 +191,7 @@ object Main {
 
       val splitRanges = splitEvents zip splitTimes.tail
 
-      val toSplit = splitRanges.find(t => secondsInActivity(t._2) == splitTime)
+      val toSplit = splitRanges.find(t => secondsInActivity(t._1.stamp) == splitTime)
 
       toSplit.map { case (beg, endTime) =>
 

src/main/scala/com/github/opengrabeso/stravalas/requests/UploadToStrava.scala
@@ -34,7 +34,7 @@ object UploadToStrava extends ProcessFile("/upload-strava") {
       resp.`type`(contentType)
     }
 
-
+    Nil
   }
 
 