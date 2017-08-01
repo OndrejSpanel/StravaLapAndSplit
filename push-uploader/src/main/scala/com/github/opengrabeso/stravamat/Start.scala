137b1fca203722fe8eb2cbb81ab435c7dc10bb03
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-08-01 16:03:35 +0200

    Introuduced push uploader (Stravamat Start).

---
+++ push-uploader/src/main/scala/com/github/opengrabeso/stravamat/Start.scala
@@ -0,0 +1,10 @@
+package com.github.opengrabeso.stravamat
+
+object Start extends App {
+
+  // open browser, ask Stravamat to perform OAuth dance
+
+
+  // local web browser used to receive the OAuth dance result
+
+}
