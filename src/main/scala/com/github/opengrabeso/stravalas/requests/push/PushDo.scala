352f1a803bea1ce53a5afb0c8bebe1d234baecc2
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-08-03 17:47:23 +0200

    Ajax pending status API working.

--- src/main/scala/com/github/opengrabeso/stravalas/requests/push/PushDo.scala
+++ src/main/scala/com/github/opengrabeso/stravalas/requests/push/PushDo.scala
@@ -17,10 +17,34 @@ object PushDo extends DefineRequest("/push-do") with ActivityRequestHandler {
           tr.activities:nth-child(even) {{background-color: #f2f2f2}}
           tr.activities:hover {{background-color: #f0f0e0}}
         </style>
+        <script src="static/ajaxUtils.js"></script>
+        <script src="static/jquery-3.2.1.min.js"></script>
       </head>
       <body>
         {bodyHeader(auth)}<h2>Receiving Suunto files ...</h2>
-        <a href="push-list-pending">List pending</a>
+        <p>
+          <span id="done"></span> of <span id="total"></span>
+        </p>
+        <a href="javascript:update();false">List pending</a>
+        <script>
+          {xml.Unparsed(
+          //language=JavaScript
+          """
+          function update() {
+            $.ajax({
+              url: "push-list-pending",
+              dataType: "xml",
+              cache: false
+            }).done( function(response) {
+              var totalFiles = $(response).find("total").first().text();
+              var doneFiles = $(response).find("done").first().text();
+              $("#total").html(totalFiles);
+              $("#done").html(doneFiles);
+            });
+          }
+          """)}
+        </script>
+
       </body>
     </html>
 
