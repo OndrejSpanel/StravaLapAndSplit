6e5985eb6e6341ed8eec065bcd29332e0cd29fe7
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-08-14 10:51:36 +0200

    Add activity merging - same sport, following soon.

--- src/main/scala/com/github/opengrabeso/stravamat/requests/Process.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/requests/Process.scala
@@ -8,6 +8,27 @@ import com.google.appengine.api.taskqueue._
 
 object Process extends DefineRequest.Post("/process") with ParseFormData {
 
+  private def follows(first: ActivityEvents, second: ActivityEvents) = {
+    val secondGap = second.secondsInActivity(first.endTime)
+    // if second starts no more than 10 minutes after the first ends, merge them
+    secondGap < 10 && secondGap > -10 * 60
+  }
+
+  private def mergeConsecutive(events: List[ActivityEvents]): List[ActivityEvents] = {
+    @scala.annotation.tailrec
+    def mergeConsecutiveRecurse(todo: List[ActivityEvents], done: List[ActivityEvents]): List[ActivityEvents] = {
+      todo match {
+        case first :: second :: tail if first.id.sportName == second.id.sportName && follows(first, second) =>
+          mergeConsecutiveRecurse(tail, first.merge(second) :: done)
+        case first :: tail =>
+          mergeConsecutiveRecurse(tail, first :: done)
+        case Nil =>
+          done
+      }
+    }
+    mergeConsecutiveRecurse(events, Nil).reverse
+  }
+
   def mergeAndUpload(auth: Main.StravaAuthResult, toMerge: Vector[ActivityEvents], sessionId: String): Int = {
     if (toMerge.nonEmpty) {
 
@@ -21,7 +42,7 @@ object Process extends DefineRequest.Post("/process") with ParseFormData {
       def filterIgnored(x: ActivityEvents) = x.isAlmostEmpty(ignoreDuration)
 
       val timelineGPS = gpsMoves.toList.filterNot(filterIgnored).sortBy(_.startTime)
-      val timelineAttr = attrMoves.toList.filterNot(filterIgnored).sortBy(_.startTime)
+      val timelineAttr = mergeConsecutive(attrMoves.toList.filterNot(filterIgnored).sortBy(_.startTime))
 
       val merged = moveslink.MovesLinkUploader.processTimelines(timelineGPS, timelineAttr)
 
