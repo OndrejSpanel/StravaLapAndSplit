a530d54ad2b407501e67d57b732d50aa250a6e1d
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-09-20 15:36:59 +0200

    Please wait / loading basics working.

--- src/main/scala/com/github/opengrabeso/stravamat/requests/SelectActivity.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/requests/SelectActivity.scala
@@ -1,16 +1,15 @@
 package com.github.opengrabeso.stravamat
 package requests
 
-import spark.{Request, Response}
 import shared.Util._
 import Main._
 import org.joda.time.{DateTime => ZonedDateTime}
 
 import scala.xml.NodeSeq
 
-abstract class SelectActivity(name: String) extends DefineRequest(name) with UploadResults {
-
+trait SelectActivityPart extends HtmlPart with ShowPending with UploadResults {
   def title: String
+
   def sources(before: ZonedDateTime): NodeSeq
   def filterListed(activity: ActivityHeader, strava: Option[ActivityId]) = true
   def ignoreBefore(stravaActivities: Seq[ActivityId]): ZonedDateTime
@@ -27,158 +26,160 @@ abstract class SelectActivity(name: String) extends DefineRequest(name) with Upl
     <script>document.write({xml.Unparsed(toRun)})</script>
   }
 
-  override def html(request: Request, resp: Response) = {
-    withAuth(request, resp) { auth =>
+  abstract override def headerPart(req: Request, auth: StravaAuthResult) = {
+    headPrefix ++
+    <title>
+      Stravamat - {title}
+    </title>
+    <style>
+      tr.activities:nth-child(even) {{background-color: #f2f2f2}}
+      tr.activities:hover {{background-color: #f0f0e0}}
+    </style>
+    <script src="static/ajaxUtils.js"></script>
+    <script src="static/timeUtils.js"></script>
+    <script src="static/jquery-3.2.1.min.js"></script> ++
+    super.headerPart(req, auth)
 
-      val session = request.session()
-
-      val stravaActivities = recentStravaActivities(auth)
-
-      startUploadSession(session)
-
-      val stagedActivities = Main.stagedActivities(auth).toVector // toVector to avoid debugging streams
-      val before = ignoreBefore(stravaActivities)
-
-      //println(s"Ignore before $before")
-
-      val recentActivities = stagedActivities.filter(_.id.startTime > before).sortBy(_.id.startTime)
-      //println(s"Staged ${stagedActivities.mkString("\n  ")}")
-      //println(s"Recent ${recentActivities.mkString("\n  ")}")
-
-      // match recent activities against Strava activities
-      // a significant overlap means a match
-      val recentToStrava = recentActivities.map { r =>
-        r -> stravaActivities.find(_ isMatching r.id)
-      }.filter((filterListed _).tupled)
-
-      <html>
-        <head>
-          {headPrefix}<title>Stravamat -
-          {title}
-        </title>
-          <style>
-            tr.activities:nth-child(even) {{background-color: #f2f2f2}}
-            tr.activities:hover {{background-color: #f0f0e0}}
-          </style>
-          <script src="static/ajaxUtils.js"></script>
-          <script src="static/timeUtils.js"></script>
-          <script src="static/jquery-3.2.1.min.js"></script>
-          <script>
-            {xml.Unparsed(
-            //language=JavaScript
-            """
-            function submitProcess() {
-              document.getElementById("upload_button").style.display = "none";
-              document.getElementById("uploads_table").style.display = "block";
-
-              var form = $("#process-form");
-              $.ajax({
-                type: form.attr("method"),
-                url: form.attr("action"),
-                data: new FormData(form[0]),
-                contentType: false,
-                cache: false,
-                processData: false,
-                success: function(response) {
-                    showResults();
-                },
-              });
-            }
-
-            function submitDelete() {
-              var form = $("#process-form");
-              $.ajax({
-                type: "post",
-                url: "delete-selected",
-                data: new FormData(form[0]),
-                contentType: false,
-                cache: false,
-                processData: false,
-                success: function(response) {
-                    // response is XML id code of the activity to redirect to
-
-                    window.location.reload(false);
-                },
-              });
-            }
-
-            function submitEdit() {
-              var form = $("#process-form");
-              $.ajax({
-                type: "post",
-                url: "merge-activity",
-                data: new FormData(form[0]),
-                contentType: false,
-                cache: false,
-                processData: false,
-                success: function(response) {
-                  var idElem = $(response).find("id");
-                  if (idElem.length > 0) {
-                    window.location = "edit-activity?id=" + idElem.first().text().trim()
-                  }
-                },
-              });
-            }
-            """
-          )}
-
-          </script>
-          </head>
-          <body>
-            {bodyHeader(auth)}{sources(before)}<h2>Activities</h2>
-            <form id="process-form" action="process" method="post" enctype="multipart/form-data">
-              <table class="activities">
-                {// find most recent Strava activity
-                val mostRecentStrava = stravaActivities.headOption.map(_.startTime)
-
-                for ((actEvents, actStrava) <- recentToStrava) yield {
-                  //println(s"  act $actEvents $actStrava")
-                  val act = actEvents.id
-                  val ignored = mostRecentStrava.exists(_ > act.endTime)
-                  // once any activity is present on Strava, do not offer upload by default any more
-                  // (if some earlier is not present, it was probably already uploaded and deleted)
-                  <tr>
-                    <td>{jsResult(jsDateRange(act.startTime, act.endTime))}</td>
-                    <td>
-                      {
-                      val detected = Main.detectSportBySpeed(actEvents.stats, act.sportName)
-                      if (act.sportName == Event.Sport.Workout) {
-                        s"$detected?"
-                      } else if (act.sportName != detected) {
-                        s"${act.sportName}->$detected"
-                      } else act.sportName
-                      //println(s"    $detected")
-                      }
-                    </td>
-                    <td>{if (actEvents.hasGPS) "GPS" else "--"}</td>
-                    <td>{if (actEvents.hasAttributes) "Rec" else "--"}</td>
-                    <td>{act.hrefLink}</td>
-                    <td>{displayDistance(act.distance)} km</td>
-                    <td>{displaySeconds(act.duration)}</td>
-                    <td>{htmlActivityAction(act.id, !ignored)}</td>
-                    <td>{actStrava.map(_.hrefLink).getOrElse(NodeSeq.Empty)}</td>
-                    <td>{act.id.toReadableString}</td>
-                  </tr>
-                }}
-              </table>
-
-          </form>
-          <button id="upload_button" onclick="submitProcess()">Process...</button>
-          {uploadResultsHtml()}
-          <button onclick="submitDelete()">Delete from Stravamat</button>
-          <button onclick="submitEdit()">Merge and edit...</button>
-          {bodyFooter}
-          <script>{xml.Unparsed(
-          //language=JavaScript
-          """
-          $("#process-form").submit(function(event) {
-            // Stop the browser from submitting the form.
-            event.preventDefault();
-          });
-          """)}
-        </script>
-        </body>
-      </html>
-    }
   }
+
+  abstract override def bodyPart(request: Request, auth: StravaAuthResult) = {
+    val session = request.session()
+
+    val stravaActivities = recentStravaActivities(auth)
+
+    startUploadSession(session)
+
+    val stagedActivities = Main.stagedActivities(auth).toVector // toVector to avoid debugging streams
+    val before = ignoreBefore(stravaActivities)
+
+    //println(s"Ignore before $before")
+
+    val recentActivities = stagedActivities.filter(_.id.startTime > before).sortBy(_.id.startTime)
+    //println(s"Staged ${stagedActivities.mkString("\n  ")}")
+    //println(s"Recent ${recentActivities.mkString("\n  ")}")
+
+    // match recent activities against Strava activities
+    // a significant overlap means a match
+    val recentToStrava = recentActivities.map { r =>
+      r -> stravaActivities.find(_ isMatching r.id)
+    }.filter((filterListed _).tupled)
+
+    super.bodyPart(request, auth) ++
+    <script>
+      {xml.Unparsed(
+      //language=JavaScript
+      """
+        function submitProcess() {
+          document.getElementById("upload_button").style.display = "none";
+          document.getElementById("uploads_table").style.display = "block";
+
+          var form = $("#process-form");
+          $.ajax({
+            type: form.attr("method"),
+            url: form.attr("action"),
+            data: new FormData(form[0]),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(response) {
+                showResults();
+            },
+          });
+        }
+
+        function submitDelete() {
+          showPending();
+          var form = $("#process-form");
+          $.ajax({
+            type: "post",
+            url: "delete-selected",
+            data: new FormData(form[0]),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(response) {
+                // response is XML id code of the activity to redirect to
+
+                window.location.reload(false);
+            },
+          });
+        }
+
+        function submitEdit() {
+          showPending();
+          var form = $("#process-form");
+          $.ajax({
+            type: "post",
+            url: "merge-activity",
+            data: new FormData(form[0]),
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(response) {
+              var idElem = $(response).find("id");
+              if (idElem.length > 0) {
+                window.location = "edit-activity?id=" + idElem.first().text().trim()
+              }
+            },
+          });
+        }
+        """
+    )}
+    </script> ++
+      {bodyHeader(auth) ++ sources(before)} ++ <h2>Activities</h2>
+      <form id="process-form" action="process" method="post" enctype="multipart/form-data">
+        <table class="activities">
+          {// find most recent Strava activity
+          val mostRecentStrava = stravaActivities.headOption.map(_.startTime)
+
+          for ((actEvents, actStrava) <- recentToStrava) yield {
+            //println(s"  act $actEvents $actStrava")
+            val act = actEvents.id
+            val ignored = mostRecentStrava.exists(_ > act.endTime)
+            // once any activity is present on Strava, do not offer upload by default any more
+            // (if some earlier is not present, it was probably already uploaded and deleted)
+            <tr>
+              <td>{jsResult(jsDateRange(act.startTime, act.endTime))}</td>
+              <td>
+                {
+                val detected = Main.detectSportBySpeed(actEvents.stats, act.sportName)
+                if (act.sportName == Event.Sport.Workout) {
+                  s"$detected?"
+                } else if (act.sportName != detected) {
+                  s"${act.sportName}->$detected"
+                } else act.sportName
+                //println(s"    $detected")
+                }
+              </td>
+              <td>{if (actEvents.hasGPS) "GPS" else "--"}</td>
+              <td>{if (actEvents.hasAttributes) "Rec" else "--"}</td>
+              <td>{act.hrefLink}</td>
+              <td>{displayDistance(act.distance)} km</td>
+              <td>{displaySeconds(act.duration)}</td>
+              <td>{htmlActivityAction(act.id, !ignored)}</td>
+              <td>{actStrava.map(_.hrefLink).getOrElse(NodeSeq.Empty)}</td>
+              <td>{act.id.toReadableString}</td>
+            </tr>
+          }}
+        </table>
+
+      </form>
+      <button id="upload_button" onclick="submitProcess()">Process...</button> ++
+      uploadResultsHtml() ++
+      <button onclick="submitDelete()">Delete from Stravamat</button>
+      <button onclick="submitEdit()">Merge and edit...</button> ++
+      bodyFooter ++
+      <script>{xml.Unparsed(
+        //language=JavaScript
+        """
+      $("#process-form").submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+      });
+      """)}
+      </script>
+  }
+
 }
+
+abstract class SelectActivity(name: String) extends DefineRequest(name) with HtmlByParts with SelectActivityPart
--- src/main/scala/com/github/opengrabeso/stravamat/requests/ShowPending.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/requests/ShowPending.scala
@@ -5,21 +5,32 @@ trait ShowPending extends HtmlPart {
 
   abstract override def bodyPart(req: Request, auth: Main.StravaAuthResult) = {
     super.bodyPart(req, auth) ++
-    <div class="modal-header">
-      <h1>Please Wait</h1>
-    </div>
-    <div class="modal-body">
-      <div id="ajax_loader">
-        <p>Loading ...</p>
+    <div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false" style="display:none">
+      <div class="modal-header">
+        <h1>Please Wait</h1>
+      </div>
+      <div class="modal-body">
+        <div id="ajax_loader">
+          <p>Loading ...</p>
+        </div>
       </div>
     </div>
     <script>
       function showPending() {{
-      $('# pleaseWaitDialog').modal();
+      $('#pleaseWaitDialog').modal();
       }}
       function hidePending() {{
-      $('# pleaseWaitDialog').modal();
+      $('#pleaseWaitDialog').modal();
       }}
     </script>
   }
+
+  abstract override def headerPart(req: Request, auth: StravaAuthResult) = {
+    super.headerPart(req, auth) ++
+    <script src="static/jquery.modal.min.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" href="static/jquery.modal.min.css" type="text/css" media="screen" />
+  }
+
+
+
 }
