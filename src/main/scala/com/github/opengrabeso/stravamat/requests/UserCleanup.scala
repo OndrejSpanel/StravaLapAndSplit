add1b6b8b8ea0a4e29f8f9872168c3d27543990d
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-09-24 17:34:09 +0200

    Limit cleanup to one item per request.

--- src/main/scala/com/github/opengrabeso/stravamat/requests/UserCleanup.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/requests/UserCleanup.scala
@@ -8,33 +8,40 @@ import RequestUtils._
 import Main._
 import shared.Util._
 
-import scala.collection.immutable
+import scala.util.control.Breaks._
+
 /**
   * User specific cleanup, requires user access tokens for Strava */
 
 @SerialVersionUID(10L)
 case class UserCleanup(auth: Main.StravaAuthResult, before: ZonedDateTime) extends DeferredTask {
-  override def run() = {
+  override def run(): Unit = {
 
     val d = Storage.enumerate(namespace.stage, auth.userId)
 
     // clean activities before "before", as those are never listed to the user
     // verify they are already stored on Strava, if not, keep then until a global expiry cleanup will handle them
     val headers = d.flatMap { a =>
-      Storage.load[ActivityHeader](namespace.stage, a, auth.userId)
-    }.filter(_.id.startTime < before).toSeq.sortBy(_.id.startTime)
+      Storage.load[ActivityHeader](namespace.stage, a, auth.userId).map(a -> _)
+    }
 
-    for (h <- headers) {
+    val headersToClean = headers.filter(_._2.id.startTime < before).toSeq.sortBy(_._2.id.startTime)
 
-      val timestamp = h.id.startTime.getMillis / 1000 - 1 * 3600
+    breakable {
+      for ((file, h) <- headersToClean) {
 
-      val uri = "https://www.strava.com/api/v3/athlete/activities"
-      val request = buildGetRequest(uri, auth.token, s"per_page=15&after=$timestamp")
+        val timestamp = h.id.startTime.getMillis / 1000 - 1 * 3600
 
-      val stravaActivities = parseStravaActivities(request.execute().getContent)
+        val uri = "https://www.strava.com/api/v3/athlete/activities"
+        val request = buildGetRequest(uri, auth.token, s"per_page=15&after=$timestamp")
 
-      if (stravaActivities.exists(_ isMatching h.id)) {
-        println(s"Will clean ${h.id}")
+        val stravaActivities = parseStravaActivities(request.execute().getContent)
+
+        if (stravaActivities.exists(_ isMatching h.id)) {
+          println(s"Cleaning stage file ${h.id} $file")
+          Storage.delete(namespace.stage, file, auth.userId)
+          break // always clean at most one item per request
+        }
       }
     }
 
