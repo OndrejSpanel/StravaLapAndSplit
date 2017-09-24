28b9019057502c68700b891ed371b6f3f39af763
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-09-24 14:28:29 +0200

    Loader background a bit more transparent.

--- src/main/scala/com/github/opengrabeso/stravamat/requests/ShowPending.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/requests/ShowPending.scala
@@ -9,6 +9,11 @@ trait ShowPending extends HtmlPart {
   abstract override def headerPart(req: Request, auth: StravaAuthResult): NodeSeq = {
     super.headerPart(req, auth) ++
     <link rel="stylesheet" href="static/css-loader.css" type="text/css" media="screen" />
+      <style>
+        .loader.is-active {{
+          background-color: rgba(0,0,0,0.7);
+        }}
+      </style>
     <script>
       function showPending() {{
         $('#loader').addClass('is-active');
