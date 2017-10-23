2b00f857a50b1a2ba1017a925617d3b3fb93ca1a
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-10-23 17:27:49 +0200

    Implemented tile caching (not used yet).

--- src/main/scala/com/github/opengrabeso/stravamat/mapbox/GetElevation.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/mapbox/GetElevation.scala
@@ -1,40 +1,61 @@
 package com.github.opengrabeso.stravamat
 package mapbox
 
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+import scala.collection.mutable
 
 object GetElevation {
   /* ScalaFromJS: 2017-10-23 09:41:46.284*/
+  class TileCache {
+    val tiles = mutable.Map.empty[(Long, Long, Long), BufferedImage]
+    def tileImage(x: Long, y: Long, z: Long): BufferedImage = {
 
-  def apply(lon: Double, lat: Double): Double = {
-    val tf = TileBelt.pointToTileFraction(lon, lat, 20)
-    val tile = tf.map(Math.floor(_).toInt)
-    val domain = "https://api.mapbox.com/v4/"
-    val source = s"""mapbox.terrain-rgb/${tile(2)}/${tile(0)}/${tile(1)}.pngraw"""
+      tiles.getOrElseUpdate((x, y, z), {
+        val domain = "https://api.mapbox.com/v4/"
+        val source = s"""mapbox.terrain-rgb/$z/$x/$y.pngraw"""
 
-    import javax.imageio.ImageIO
-    // request
-    val pars = Map("access_token" -> Main.secret.mapboxToken)
+        // request
+        val pars = Map("access_token" -> Main.secret.mapboxToken)
 
-    val request = RequestUtils.buildGetRequest(domain + source, pars)
+        val request = RequestUtils.buildGetRequest(domain + source, pars)
 
-    val response = request.execute().getContent
+        val response = request.execute().getContent
 
-    // load PNG
-    val image = ImageIO.read(response)
+        // load PNG
+        val image = ImageIO.read(response)
 
-    val xp = tf(0) - tile(0)
-    val yp = tf(1) - tile(1)
-    val x = Math.floor(xp * image.getWidth).toInt
-    val y = Math.floor(yp * image.getHeight).toInt
+        image
+      })
+    }
 
-    val rgb = image.getRGB(x, y)
+    def apply(lon: Double, lat: Double): Double = {
+      // TODO: four point bilinear interpolation
+      val tf = TileBelt.pointToTileFraction(lon, lat, 20)
+      val tile = tf.map(Math.floor(_).toLong)
 
-    val R = (rgb >> 16) & 0xff
-    val G = (rgb >> 8) & 0xff
-    val B = (rgb >> 0) & 0xff
-    val height = -10000 + (R * 256 * 256 + G * 256 + B) * 0.1
+      val image = tileImage(tile(0), tile(1), tile(2))
 
-    height
+      val xp = tf(0) - tile(0)
+      val yp = tf(1) - tile(1)
+      val x = Math.floor(xp * image.getWidth).toInt
+      val y = Math.floor(yp * image.getHeight).toInt
+
+      val rgb = image.getRGB(x, y)
+
+      val R = (rgb >> 16) & 0xff
+      val G = (rgb >> 8) & 0xff
+      val B = (rgb >> 0) & 0xff
+      val height = -10000 + (R * 256 * 256 + G * 256 + B) * 0.1
+
+      height
+    }
+
+  }
+
+  def apply(lon: Double, lat: Double, cache: TileCache = new TileCache): Double = {
+    cache(lon, lat)
   }
 }
 
