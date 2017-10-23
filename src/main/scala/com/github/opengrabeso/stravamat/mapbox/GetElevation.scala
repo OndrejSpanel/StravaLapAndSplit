d64b5600cadcd2928d8e447588ecd6db71cdf5c0
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-10-23 12:15:14 +0200

    Ported get pixel API to Scala.

--- src/main/scala/com/github/opengrabeso/stravamat/mapbox/GetElevation.scala
+++ src/main/scala/com/github/opengrabeso/stravamat/mapbox/GetElevation.scala
@@ -1,26 +1,40 @@
-package com.github.opengrabeso.stravamat.mapbox
+package com.github.opengrabeso.stravamat
+package mapbox
+
 
 object GetElevation {
   /* ScalaFromJS: 2017-10-23 09:41:46.284*/
 
-  def apply(lon: Double, lat: Double, cb: (Unit, Unit) => Any) = {
+  def apply(lon: Double, lat: Double): Double = {
     val tf = TileBelt.pointToTileFraction(lon, lat, 20)
     val tile = tf.map(Math.floor)
     val domain = "https://api.mapbox.com/v4/"
     val source = """mapbox.terrain-rgb///.pngraw"""
-    val url = """?access_token="""
-    getPixels(url, (err, pixels) => {
-      if (err) return cb(err)
-      val xp = tf(0) - tile(0)
-      val yp = tf(1) - tile(1)
-      val x = Math.floor(xp * pixels.shape(0))
-      val y = Math.floor(yp * pixels.shape(1))
-      val R = pixels.get(x, y, 0)
-      val G = pixels.get(x, y, 1)
-      val B = pixels.get(x, y, 2)
-      val height = -10000 + (R * 256 * 256 + G * 256 + B) * 0.1
-      cb(null, height)
-    })
+
+    import javax.imageio.ImageIO
+    // request
+    val pars = Map("access_token" -> Main.secret.mapboxToken)
+
+    val request = RequestUtils.buildGetRequest(domain + source, pars)
+
+    val response = request.execute().getContent
+
+    // load PNG
+    val image = ImageIO.read(response)
+
+    val xp = tf(0) - tile(0)
+    val yp = tf(1) - tile(1)
+    val x = Math.floor(xp * image.getWidth).toInt
+    val y = Math.floor(yp * image.getHeight).toInt
+
+    val rgb = image.getRGB(x, y)
+
+    val R = (rgb >> 16) & 0xff
+    val G = (rgb >> 8) & 0xff
+    val B = (rgb >> 0) & 0xff
+    val height = -10000 + (R * 256 * 256 + G * 256 + B) * 0.1
+
+    height
   }
 }
 
