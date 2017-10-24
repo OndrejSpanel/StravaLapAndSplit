28d265f825607cf88bdf0abc77d718871e5d2c06
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2017-10-24 10:22:37 +0200

    Fix Mount Everest test (for real this time).

--- src/test/scala/com/github/opengrabeso/stravamat/mapbox/GetElevationTest.scala
+++ src/test/scala/com/github/opengrabeso/stravamat/mapbox/GetElevationTest.scala
@@ -8,13 +8,14 @@ class GetElevationTest extends FunSuite {
   implicit val doubleEquality = TolerantNumerics.tolerantDoubleEquality(0.1)
 
   test("test Mount Everest") {
+    import GetElevation.DefaultThreadFactory._
     val summit = GetElevation(86.925313, 27.988730)
     assert(summit >= 8742.0)
     assert(summit <= 8743.0)
 
     val summitRange = new GetElevation.TileCache().possibleRange(86.925313, 27.988730)
-    assert(summitRange._1 < 8742.8)
-    assert(summitRange._2 > 8742.8)
+    assert(summitRange._1 <= 8742.7)
+    assert(summitRange._2 >= 8742.8)
 
   }
 
