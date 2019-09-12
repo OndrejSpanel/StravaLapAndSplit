98fc0c03655a09ff3d72fde75f9e0f6ebf190973
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-09-12 14:50:35 +0200

    Implement async response possibility.

--- src/main/scala/com/github/opengrabeso/mixtio/rest/RestAPIServer.scala
+++ src/main/scala/com/github/opengrabeso/mixtio/rest/RestAPIServer.scala
@@ -1,8 +1,31 @@
 package com.github.opengrabeso.mixtio
 package rest
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object RestAPIServer extends RestAPI {
-  def identity(in: String) = Future.successful(in)
+
+  def syncResponse[T](t: =>T): Future[T] = {
+    Future.successful(t)
+  }
+  // note: Not very efficient, there is no thread pool. Use only for heavy-weight tasks
+  def asyncResponse[T](t: =>T): Future[T] = {
+    val threadFactory = com.google.appengine.api.ThreadManager.currentRequestThreadFactory()
+
+    implicit object EC extends ExecutionContext {
+      def execute(runnable: Runnable) = {
+        val t = threadFactory.newThread(runnable)
+        t.start()
+      }
+      def reportFailure(cause: Throwable) = cause.printStackTrace()
+    }
+    Future {
+      t
+    }
+
+  }
+
+  def identity(in: String) = {
+    syncResponse(in)
+  }
 }
