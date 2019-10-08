682a4d1b3690e30e0ecfcae500f4ead8dc800328
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-10-08 15:04:13 +0200

    Working on Embedded Jetty to make local debugging more reliable (GCS not working yet).

--- backend/src/main/scala/com/github/opengrabeso/mixtio/ServletRouting.scala
+++ backend/src/main/scala/com/github/opengrabeso/mixtio/ServletRouting.scala
@@ -1,48 +1,46 @@
 package com.github.opengrabeso.mixtio
 
+import java.io.{File, FileInputStream}
+import org.apache.commons.io.IOUtils
+
 import spark.{Request, Response, Route}
 import spark.servlet.SparkApplication
 import spark.Spark._
 
 object ServletRouting {
+  import requests._
+
   def route(path: String)(handleFunc: (Request, Response) => AnyRef): Route = {
     new Route(path) {
       override def handle(request: Request, response: Response) = handleFunc(request, response)
     }
   }
 
-}
+  val handlers: Seq[DefineRequest] = Seq(
+    IndexHtml, LogOut,
 
-class ServletRouting extends SparkApplication {
+    FrontendStyle,
+    FrontendScript, UdashApp,
 
-  import ServletRouting._
+    push.Ping, push.PushStart, push.PushDo,
+
+    push.upload.PutStart, push.upload.PutFile, push.upload.PutDigest, push.upload.ListPending,
+
+    ManageActivities, LoadFromStrava, ActivityFromStrava, Upload, GetFiles, Staging,
+    MergeAndEditActivity, EditActivity, Download, UploadToStrava, ProcessEdit,
+
+    Process, DeleteSelected, CheckUploadStatus,
+
+    SettingsPage,
+
+    Cleanup, ConvertOldData,
+
+    RouteData
+  )
 
   def init() {
     // add any type derived from DefineRequest here
     // solution with reflection is elegant, but overcomplicated (and hard to get working with Google App Engine) and slow
-    import requests._
-    val handlers: Seq[DefineRequest] = Seq(
-      IndexHtml, LogOut,
-
-      FrontendStyle,
-      FrontendScript, UdashApp,
-
-      push.Ping, push.PushStart, push.PushDo,
-
-      push.upload.PutStart, push.upload.PutFile, push.upload.PutDigest, push.upload.ListPending,
-
-      ManageActivities, LoadFromStrava, ActivityFromStrava, Upload, GetFiles, Staging,
-      MergeAndEditActivity, EditActivity, Download, UploadToStrava, ProcessEdit,
-
-      Process, DeleteSelected, CheckUploadStatus,
-
-      SettingsPage,
-
-      Cleanup, ConvertOldData,
-
-      RouteData
-    )
-
     def addPage(h: DefineRequest) = {
       val r = route(h.handleUri) (h.apply)
       h.method match {
@@ -53,7 +51,73 @@ class ServletRouting extends SparkApplication {
       }
     }
 
-    handlers.foreach (addPage)
+    handlers.foreach(addPage)
   }
+
+  /** Note: server started this way has some limitations
+    * */
+  def main(args: Array[String]): Unit = {
+    // start embedded Spark / Jetty server
+    // defining routing will start init on its own
+
+    object RestRoute extends Route("/rest/*") {
+      object servlet extends rest.ServletRestAPIRest
+      def handle(request: Request, response: Response) = {
+        // maybe there is some more portable way, currently we do servlet path change only for Jetty requests
+        // when running embedded, we are sure to be running Jetty, therefore this should work fine
+        request.raw match {
+          case req: org.eclipse.jetty.server.Request =>
+            val pathInfo = req.getPathInfo
+            if (pathInfo.startsWith("/rest")) {
+              req.setServletPath("/rest")
+            }
+          case _ =>
+        }
+        servlet.service(request.raw, response.raw)
+        response
+      }
+    }
+
+    object StaticRoute extends Route("/static/*") {
+      def handle(request: Request, response: Response) = {
+        val filename = request.splat().head
+        val path = "backend/web/static/" + filename
+        val stream = new FileInputStream(new File(path))
+        try {
+          val out = response.raw.getOutputStream
+          IOUtils.copy(stream, out)
+          out.close()
+        } finally {
+          stream.close()
+        }
+        response
+      }
+    }
+
+    get(RestRoute)
+    post(RestRoute)
+    put(RestRoute)
+    delete(RestRoute)
+    patch(RestRoute)
+    head(RestRoute)
+    connect(RestRoute)
+    options(RestRoute)
+    trace(RestRoute)
+
+    get(StaticRoute)
+
+    init()
+  }
+
+}
+
+class ServletRouting extends SparkApplication {
+
+  def init() = {
+
+    ServletRouting.init()
+
+  }
+
 
 }
